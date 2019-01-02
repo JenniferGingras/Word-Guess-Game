ed2a326c2f19657f713f9dc61bfd7dc0d3651aa1
Jennifer Gingras <jennifergingras@tests-MacBook-Pro-2.local>
2019-01-01 21:51:24 -0500

    still crude, but working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,9 +18,8 @@ var wrongLetter = [];
 var numGuess = 8;
 var wins = 0;
 var losses = 0;
-// variables which get values during gameplay
-var keyGuess;
-var randomWord;
+var randomWord = "";
+var letterKey = "";
 // var startBtn = document.getElementById('startBtn');
 
 
@@ -28,73 +27,110 @@ var randomWord;
 //-----------------------
 
 // START/RESTART - chooses a new word and sets counters/arrays back to starting point
-// function gameSet() {
+function gameSet() {
 
   // set counters at startpoint
   numGuess = 8;
-  wrongLetter = [];
 
   // computer selects random word from array
   randomWord = wordArr[Math.floor(Math.random() * wordArr.length)];
 
+  // test randomWord
+  console.log(randomWord);
+
   // turn letters in randomWord into string
   randomWordLetters = randomWord.split("");
+
+  // set array for letters
+  answerArr = [];
 
   // turn letters in word into asterisks
   for (var i = 0; i < randomWordLetters.length; i++) {
     answerArr.push("*");
   }
+
+  // test answerArr with asterisks
+  console.log(answerArr);
+
+  // set empty array for wrong guesses
+  wrongLetter = [];
+
   // display 
   document.getElementById("letters").innerHTML = answerArr.join(" ");
   document.getElementById("wrongLetters").innerHTML = wrongLetter.join(" ");
   document.getElementById("guessCount").innerHTML = numGuess;
   document.getElementById("wins").innerHTML = wins;
   document.getElementById("losses").innerHTML = losses;
-// }
-
-
-// USER INPUT
-document.onkeyup = function (event) {
-  keyGuess = event.key;
-  // console.log(keyGuess)
 }
 
-
 // CHECK GUESSED LETTERS
-function checkLetter(letter) {
-  if (keyGuess >= 65 && keyGuess <= 90) { // check to make sure it's a letter
+function checkLetter(keyGuess) {
+  // I don't understand why I need this true/false part, but it was in examples I found and when I take it away again stuff breaks.
+  var letter = false;
+  for (var i = 0; i < randomWordLetters.length; i++) {
+    if (randomWord[i] === keyGuess) {
+      letter = true;
+    }
+  }
+  if (letter) {
     for (var i = 0; i < randomWordLetters.length; i++) { // check the guessed letter against letters in the word
-      if (keyGuess === randomWordLetters[i]) { // if guessed letter is in word 
+      if (randomWord[i] === keyGuess) { // if guessed letter is in word 
         answerArr[i] = keyGuess; // add letter to answer
-      } else { // if guessed letter is not in word 
-        wrongLetter.push(keyGuess); // add letter to wrongLetter array 
-        numGuess--; // subtract 1 from guesses
       }
     }
+    // test
+    console.log(answerArr);
+
+  } else { // if guessed letter is not in word 
+    wrongLetter.push(keyGuess); // add letter to wrongLetter array 
+    numGuess--; // subtract 1 from guesses
   }
 }
 
+
 // FINISH A WORD ROUND
-function roundEnd() {
+function afterGuess() {
+  // update HTML 
+  document.getElementById("letters").innerHTML = answerArr.join(" ");
+  document.getElementById("wrongLetters").innerHTML = wrongLetter.join(" ");
+  document.getElementById("guessCount").innerHTML = numGuess;
   // game lost
   if (numGuess === 0) {
     // Add (1) to losses
     losses++;
+    document.getElementById("losses").innerHTML = losses;
     // display losing message - change to image after figuring out basic functionality
     alert("Aw, man. Get it together!")
+    gameSet();
   }
   // game won
-  // if () {
-  // Add (1) to wins
-  wins++;
-  // disply win message - change to image after figuring out basic functionality
-  alert("Far out!")
+  else if (randomWordLetters.toString() === answerArr.toString()) {
+    // Add (1) to wins
+    wins++;
+    document.getElementById("wins").innerHTML = wins;
+    // disply win message - change to image after figuring out basic functionality
+    alert("Far out!")
+
+    //restart the game
+    gameSet();
+  }
 }
-// }
 
 // PLAYING GAME?
 //----------------------
+// call the start of game 
+gameSet();
+// startBtn.addEventListener('click', gameSet()) {
+// document.getElementById('startBtn').innerHTML
+// }
 
-// startBtn.addEventListener('click', gameSet);
+// USER INPUT
+document.onkeyup = function (event) {
+  letterKey = event.key
+  console.log(letterKey)
 
-// checkLetter();
+  // call the checkLetter function
+  checkLetter(letterKey);
+  // call the afterGuess function
+  afterGuess();
+}
